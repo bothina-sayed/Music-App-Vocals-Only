1677f549c01b794a894a9be5340c0c5495985d31
Bothina Sayed <bothinasaed0@gmail.com>
2023-04-25 22:00:23 +0200

    update 1

---
+++ MusicList.js
@@ -0,0 +1,45 @@
+let allMusic=[
+    {
+        name:"Always be There",
+        artist:"Maher Zain",
+        img:"music-1",
+        src:"music-1"
+    },
+    {
+        name:"Assalamu Alayka",
+        artist:"Maher Zain",
+        img:"music-2",
+        src:"music-2"
+    },
+    {
+        name:"For the Rest of My Life",
+        artist:"Maher Zain",
+        img:"music-3",
+        src:"music-3"
+    },
+    {
+        name:"Insha Allah",
+        artist:"Maher Zain",
+        img:"music-4",
+        src:"music-4"
+    },
+    {
+        name:"Samah",
+        artist:"Maher Zain",
+        img:"music-5",
+        src:"music-5"
+    },
+    {
+        name:"Thank you Allah",
+        artist:"Maher Zain",
+        img:"music-6",
+        src:"music-6"
+    },
+    {
+        name:"The Chosen One",
+        artist:"Maher Zain",
+        img:"music-7",
+        src:"music-7"
+    }
+    
+]
